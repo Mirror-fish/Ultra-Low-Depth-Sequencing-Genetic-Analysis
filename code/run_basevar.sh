eb3770d34bbb9917071f6e602c4b94b53dd1c440
Mirror-fish <82803601+Mirror-fish@users.noreply.github.com>
2024-10-15 18:53:40 +0800

    Delete code directory

--- code/run_basevar.sh
+++
@@ -1,6 +0,0 @@
-BaseVarC basetype \
---input bam_forge.list \
---reference Homo_sapiens_assembly38.fasta \
---region chr12:1000001-1500000 \
---output ./chr12-1000001-1500000.10 \
---batch 10 --rerun 
