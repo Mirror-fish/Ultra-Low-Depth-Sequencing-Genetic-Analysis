72ebc8de7b60f7f4a3842496e4bbc73a2391a946
Mirror-fish <82803601+Mirror-fish@users.noreply.github.com>
2024-10-17 11:24:42 +0800

    Update 3.5.run_GWAS.sh

--- code/3.GWAS_analysis/3.5.run_GWAS.sh
+++ code/3.GWAS_analysis/3.5.run_GWAS.sh
@@ -3,8 +3,8 @@
 plink2=/path_to/plink2
 
 $plink2 \
---pfile /path_to/3.GWAS_analysis/final/STITCH 'vzs' \
---read-freq /path_to/3.GWAS_analysis/final/STITCH.afreq \
+--pfile /path_to/GWAS_analysis/final/STITCH 'vzs' \
+--read-freq /path_to/GWAS_analysis/final/STITCH.afreq \
 --glm \
 --maf 0.05 \
 --hwe 1e-6 \
@@ -12,13 +12,13 @@ $plink2 \
 --pheno ./phenotype_genaral/pheno.csv \
 --pheno-name pheno1 \
 --pheno-quantile-normalize \
---covar /path_to/3.GWAS_analysis/final/GWAS_pca_5.eigenvec \
+--covar /path_to/GWAS_analysis/final/GWAS_pca_5.eigenvec \
 --covar-variance-standardize \
---out /path_to/3.GWAS_analysis/final/STITCH.pheno_1
+--out /path_to/GWAS_analysis/final/STITCH.pheno_1
 
 $plink2 \
---pfile /path_to/3.GWAS_analysis/final/STITCH 'vzs' \
---read-freq /path_to/3.GWAS_analysis/final/STITCH.afreq \
+--pfile /path_to/GWAS_analysis/final/STITCH 'vzs' \
+--read-freq /path_to/GWAS_analysis/final/STITCH.afreq \
 --glm \
 --maf 0.05 \
 --hwe 1e-6 \
@@ -26,12 +26,12 @@ $plink2 \
 --pheno ./phenotype_genaral/pheno.csv \
 --pheno-name pheno2 \
 --pheno-quantile-normalize \
---covar /path_to/3.GWAS_analysis/final/GWAS_pca_5.eigenvec \
+--covar /path_to/GWAS_analysis/final/GWAS_pca_5.eigenvec \
 --covar-variance-standardize \
---out /path_to/3.GWAS_analysis/final/STITCH.pheno_2
+--out /path_to/GWAS_analysis/final/STITCH.pheno_2
 
-for i in `ls /path_to/3.GWAS_analysis/final/*.linear`; do head $i -n1 >> $i.add && grep 'ADD' $i >> $i.add ; done
-for i in `ls /path_to/3.GWAS_analysis/final/*.logistic.hybrid`; do head $i -n1 >> $i.add && grep 'ADD' $i >> $i.add ; done
+for i in `ls /path_to/GWAS_analysis/final/*.linear`; do head $i -n1 >> $i.add && grep 'ADD' $i >> $i.add ; done
+for i in `ls /path_to/GWAS_analysis/final/*.logistic.hybrid`; do head $i -n1 >> $i.add && grep 'ADD' $i >> $i.add ; done
 
 
 
