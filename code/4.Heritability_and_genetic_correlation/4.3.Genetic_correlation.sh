82468fd483a95d367a895ed53708c59c57c7b41c
Mirror-fish <82803601+Mirror-fish@users.noreply.github.com>
2024-10-17 16:45:52 +0800

    Update 4.3.Genetic_correlation.sh

--- code/4.Heritability_and_genetic_correlation/4.3.Genetic_correlation.sh
+++ code/4.Heritability_and_genetic_correlation/4.3.Genetic_correlation.sh
@@ -8,25 +8,6 @@ input_path=$1
 
 input_path=${input_path}/${pheno_name}
 
-# get_size
-sample_size=$(awk 'NR==2{print $2}' ${input_path}/${pheno_name}.ldsc)
-echo "${pheno_name}, sample_size: ${sample_size}"
-
-# ldsc 2 munge
-./envs/python27/bin/python2.7 \
-./software/ldsc-master/munge_sumstats.py \
---sumstats ${input_path}/${pheno_name1}.ldsc \
---N $sample_size \
---out ${input_path}/${pheno_name1} \
---merge-alleles /path_to/gwas.snplist #extract from summary statistics 
-
-./envs/python27/bin/python2.7 \
-./software/ldsc-master/munge_sumstats.py \
---sumstats ${input_path}/${pheno_name2}.ldsc \
---N $sample_size \
---out ${input_path}/${pheno_name2} \
---merge-alleles /path_to/gwas.snplist #extract from summary statistics 
-
 #genetic correlation
 ./envs/python27/bin/python2.7 \
 ./software/ldsc-master/ldsc.py \
