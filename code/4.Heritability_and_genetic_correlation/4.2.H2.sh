3dc0b289bab9289fb6ddb8658ae204ceb3b201ac
Mirror-fish <82803601+Mirror-fish@users.noreply.github.com>
2024-10-17 14:05:24 +0800

    Update 4.2.H2.sh

--- code/4.Heritability_and_genetic_correlation/4.2.H2.sh
+++ code/4.Heritability_and_genetic_correlation/4.2.H2.sh
@@ -12,16 +12,16 @@ sample_size=$(awk 'NR==2{print $2}' ${input_path}/${pheno_name}.ldsc)
 echo "${pheno_name}, sample_size: ${sample_size}"
 
 # ldsc 2 munge
-./envs/python27/bin/python2.7 \
-./software/ldsc-master/munge_sumstats.py \
+/path_to/python2.7 \
+/path_to/ldsc-master/munge_sumstats.py \
 --sumstats ${input_path}/${pheno_name}.ldsc \
 --N $sample_size \
 --out ${input_path}/${pheno_name} \
 --merge-alleles /path_to/gwas.snplist #extract from summary statistics 
 
 # munge 2 h2
-./envs/python27/bin/python2.7 \
-./software/ldsc-master/ldsc.py \
+/path_to/python2.7 \
+/path_to/ldsc-master/ldsc.py \
 --h2 ${input_path}/${pheno_name}.sumstats.gz \
 --ref-ld-chr /path_to/eas_ldscores/ \
 --w-ld-chr /path_to/LDSC/eas_ldscores/ \
