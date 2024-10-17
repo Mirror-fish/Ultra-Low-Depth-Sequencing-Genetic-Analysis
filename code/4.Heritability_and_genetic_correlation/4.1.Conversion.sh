bfc01a10a40633cf61479991bd3e710ea6759f79
Mirror-fish <82803601+Mirror-fish@users.noreply.github.com>
2024-10-17 11:45:10 +0800

    Update 4.1.Conversion.sh

--- code/4.Heritability_and_genetic_correlation/4.1.Conversion.sh
+++ code/4.Heritability_and_genetic_correlation/4.1.Conversion.sh
@@ -1,3 +1,7 @@
+#Please note that this file requires external parameter
+#After specify the arguments, you can run this code as:
+#bash 4.1.Conversion.sh /path_to/pheno1.add pheno1 /output_folder
+
 glm_add=$1
 name=$2
 ofile=$3
@@ -7,8 +11,8 @@ mkdir ${ofile}
 ofiles=${ofile}/${name}
 
 # glm 2 ss
-/path_to/python ./toolset/glm_2_ss.py $glm_add ${ofiles}/${name}.ss
+/path_to/python ./glm_2_ss.py $glm_add ${ofiles}/${name}.ss
 
 # ss 2 ldsc
-/path_to/python ./toolset/ss_2_ldsc.py ${ofiles}/${name}.ss ${ofiles}/${name}.ldsc
+/path_to/python ./ss_2_ldsc.py ${ofiles}/${name}.ss ${ofiles}/${name}.ldsc
 
