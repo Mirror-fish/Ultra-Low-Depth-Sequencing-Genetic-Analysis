61823dee7e3a36c609876df30b4cd551969f6d18
Mirror-fish <82803601+Mirror-fish@users.noreply.github.com>
2024-10-17 10:50:27 +0800

    Update 3.3.Plink_load_forBaseVarC.sh

--- code/3.GWAS_analysis/3.3.Plink_load_forBaseVarC.sh
+++ code/3.GWAS_analysis/3.3.Plink_load_forBaseVarC.sh
@@ -3,9 +3,9 @@
 plink2=/path_to/plink2
 
 #the output vcf file of 3.2.Merge.sh
-merge_out=/path_to/3.GWAS_analysis/final/BaseVarC.vcf.gz
+merge_out=/path_to/GWAS_analysis/final/BaseVarC.vcf.gz
 
 $plink2 \
 --pca 5 \
 --vcf $merge_out \
---out /path_to/3.GWAS_analysis/final/GWAS_pca_5
+--out /path_to/GWAS_analysis/final/GWAS_pca_5
