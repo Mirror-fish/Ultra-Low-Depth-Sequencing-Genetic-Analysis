1bc13a13d0c1f55f97c38fb34c03b5976ff6b2f3
Mirror-fish <82803601+Mirror-fish@users.noreply.github.com>
2025-07-22 11:49:11 +0800

    Update 3.5.1.kinship_analysis.sh

--- code/3.Genome-wide_association_study_using_PLINK2/3.5.1.kinship_analysis.sh
+++ code/3.Genome-wide_association_study_using_PLINK2/3.5.1.kinship_analysis.sh
@@ -5,5 +5,5 @@ plink2=/path_to/plink2
 $plink2 \
 --pfile /path_to/GWAS_analysis/final/STITCH 'vzs' \
 --read-freq /path_to/GWAS_analysis/final/STITCH.afreq \
---make-king triangle bin\
+--make-king triangle bin \
 --out /path_to/GWAS_analysis/final/kinship
